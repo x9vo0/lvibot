4a512a0784421575fa7c249276f7e0bdc2801f0c
x9vo0 <96320637+x9vo0@users.noreply.github.com>
2022-08-06 01:14:58 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -43,7 +43,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                text="اشترك هنا", url="https://t.me/Dec_p"
+                text="اشترك هنا", url="https://t.me/s_a_s_a9"
             ),                        
         ],        
     ]
@@ -52,10 +52,10 @@ force_btn = InlineKeyboardMarkup(
 async def check_is_joined(message):    
     try:
         userid = message.from_user.id
-        status = await app.get_chat_member("Dec_p", userid)
+        status = await app.get_chat_member("s_a_s_a9", userid)
         return True
     except Exception:
-        await message.reply_text("*انت لست مشترك في قناة البوت @Dec_p ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        await message.reply_text("*انت لست مشترك في قناة البوت @s_a_s_a9 ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
         return False
 
 # Command
